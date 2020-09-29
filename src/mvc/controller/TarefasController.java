312976c95e6e2838e333680d0cfb34ae61cd5f03
Larissa Amaral <larissa95.amaral@gmail.com>
2020-09-29 11:27:45 -0300

    initial

---
+++ src/mvc/controller/TarefasController.java
@@ -0,0 +1,13 @@
+package mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TarefasController {
+	@RequestMapping("/")
+	public String execute() {
+		System.out.println("Lógica do MVC");
+		return "info";
+	}
+}
